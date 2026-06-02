dac40f008b5fe575561a2e8dfcfe927429d3d040
alexis <alexismurugan@gmail.com>
2026-06-02 16:27:59 +0200

    add logo and SafeWay's colors

--- src/main/java/com/app/view/HomePage.java
+++ src/main/java/com/app/view/HomePage.java
@@ -3,6 +3,9 @@ package com.app.view;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import com.app.controller.MainController;
 
 public class HomePage extends VBox {
@@ -11,11 +14,19 @@ public class HomePage extends VBox {
 
         this.getStyleClass().add("home-page");
 
+        Image img = new Image(getClass().getResourceAsStream("/logo.png"));
+
+        ImageView imageView = new ImageView(img);
+
+        imageView.setFitWidth(500);
+        imageView.setPreserveRatio(true);
+
         Button go = new Button("Go Graph");
+
 
         go.setOnAction(e -> controller.showGraph());
         go.getStyleClass().add("primary-button");
 
-        getChildren().add(go);
+        getChildren().addAll(imageView, go);
     }
 }
